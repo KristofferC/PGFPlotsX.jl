f99823bb9642ee6a5ce8bb20fad1ca2068b76c00
Tamas K. Papp <tkpapp@gmail.com>
2018-01-16 13:38:38 +0100

    Added tests for the @pgf macro.

---
+++ test/test_macros.jl
@@ -0,0 +1,31 @@
+# test that the @pfg macro is expanded correctly
+
+module TestModule
+using PGFPlotsX: @pgf
+testpgf() = @pgf { ymax = 1 }
+end
+
+@testset "pgf escape" begin
+    # just testing that it is escaped and evaluated
+    @test TestModule.testpgf() ≠ nothing
+end
+
+
+# test the @pgf macro
+
+od(args...) =  DataStructures.OrderedDict{Any, Any}(args...)
+
+@testset "pgf tests" begin
+    a = 1
+    b = 2
+    @test pgf.@pgf { xmax = a + b, title = "42", justkey } ==
+        od("xmax" => 3, "title" => "42", "justkey" => nothing)
+    f(x...) = tuple(x...)
+    @test pgf.@pgf f({ look, we, are = f(1, 2, 3),
+                       nesting = {
+                           stuff = 9
+                       }}) == (od("look" => nothing,
+                                  "we" => nothing,
+                                  "are" => (1, 2, 3),
+                                  "nesting" => od("stuff" => 9)), )
+end
