1f1f83fa02321ceebfd37c570b9049d468001119
Fredrik Ekre <ekrefredrik@gmail.com>
2019-03-17 11:12:12 +0100

    pass --code-coverage to doc build process

--- test/runtests.jl
+++ test/runtests.jl
@@ -27,8 +27,9 @@ if get(ENV, "TRAVIS_JULIA_VERSION", nothing) == "1.0"
     cd(joinpath(@__DIR__, "..")) do
         withenv("JULIA_LOAD_PATH" => nothing) do
             cmd = `$(Base.julia_cmd()) --depwarn=no --color=yes --project=docs/`
+            coverage = Base.JLOptions().code_coverage == 0 ? "none" : "user"
             run(`$(cmd) -e 'using Pkg; Pkg.instantiate()'`)
-            run(`$(cmd) docs/make.jl`)
+            run(`$(cmd) --code-coverage=$(coverage) docs/make.jl`)
         end
     end
 end
