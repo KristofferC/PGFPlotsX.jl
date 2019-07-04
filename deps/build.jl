7204157c73b28db2cd8fb65a9e5113d8a85d1e83
David AW Barton <david.barton@bristol.ac.uk>
2019-07-04 12:38:27 +0100

    Added pdftocairo as an alternative to pdf2svg and pdftopnm

--- deps/build.jl
+++ deps/build.jl
@@ -24,33 +24,51 @@ else
                  "the correct paths are set then run Pkg.build(\"PGFPLotsX\")"))
 end
 
-print(stderr, "Looking for pdftoppm...")
-have_pdftoppm =  try success(`pdftoppm  -v`); catch; false; end
-println(stderr, "   ", have_pdftoppm ? OK : X)
-if !have_pdftoppm
-    @warn(string("Did not find `pdftoppm`, png output will be disabled. Install `pdftoppm` ",
-                "and run Pkg.build(\"PGFPLotsX\") to enable"))
+print(stderr, "Looking for pdftocairo...")
+have_pdftocairo =  try success(`pdftocairo  -v`); catch; false; end
+println(stderr, "   ", have_pdftocairo ? OK : X)
+
+if have_pdftocairo
+    have_pdftoppm = true
+    default_pdftoppm = `pdftocairo`
+    have_pdf2svg = true
+    default_pdftosvg = `pdftocairo -svg -l 1`
+else
+    print(stderr, "Looking for pdftoppm...")
+    have_pdftoppm =  try success(`pdftoppm  -v`); catch; false; end
+    println(stderr, "   ", have_pdftoppm ? OK : X)
+    if have_pdftoppm
+        default_pdftoppm = `pdftoppm`
+    else
+        have_pdftoppm
+        @warn(string("Did not find `pdftocairo` or `pdftoppm`, png output will be disabled. Install `pdftocairo` or `pdftoppm` ",
+                    "and run Pkg.build(\"PGFPLotsX\") to enable"))
+    end
+
+    print(stderr, "Looking for pdf2svg...")
+    pdfpath = joinpath(@__DIR__, "pdf2svg.pdf")
+    svgpath = joinpath(@__DIR__, "pdf2svg.svg")
+    have_pdf2svg = try success(`pdf2svg $pdfpath $svgpath`); catch; false; end
+    println(stderr, "    ", have_pdf2svg ? OK : X)
+    if have_pdf2svg
+        default_pdftosvg = `pdf2svg`
+    else
+        @warn(string("Did not find `pdftocairo` or `pdf2svg`, svg output will be disabled. Install `pdftocairo` or `pdf2svg` ",
+                    "and run Pkg.build(\"PGFPLotsX\") to enable"))
+    end
 end
 
-print(stderr, "Looking for pdf2svg...")
-pdfpath = joinpath(@__DIR__, "pdf2svg.pdf")
-svgpath = joinpath(@__DIR__, "pdf2svg.svg")
-have_pdf2svg = try success(`pdf2svg $pdfpath $svgpath`); catch; false; end
-println(stderr, "    ", have_pdf2svg ? OK : X)
-if !have_pdf2svg
-    @warn(string("Did not find `pdf2svg`, svg output will be disabled. Install `pdf2svg` ",
-                "and run Pkg.build(\"PGFPLotsX\") to enable"))
-end
-
-if !have_pdf2svg && !have_pdftoppm
-    @warn(string("Found neither pdf2svg or pdftoppm, figures will not be viewable in Jupyter or Juno"))
+if !have_pdftocairo && !have_pdf2svg && !have_pdftoppm
+    @warn(string("Found none of pdftocairo, pdf2svg, or pdftoppm; figures will not be viewable in Jupyter or Juno"))
 end
 
 
 open(joinpath(@__DIR__, "deps.jl"), "w") do f
     println(f, "DEFAULT_ENGINE = \"", default_engine, "\"")
     println(f, "HAVE_PDFTOPPM = ", have_pdftoppm)
+    println(f, "DEFAULT_PDFTOPPM = ", default_pdftoppm)
     println(f, "HAVE_PDFTOSVG = ", have_pdf2svg)
+    println(f, "DEFAULT_PDFTOSVG = ", default_pdftosvg)
 end
 
 
