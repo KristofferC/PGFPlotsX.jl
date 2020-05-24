4862ecee1cf18f64ebe98eb0772d4ffb4dacdc23
KristofferC <kcarlsson89@gmail.com>
2020-05-24 10:59:44 +0200

    use optlevel 1

--- src/PGFPlotsX.jl
+++ src/PGFPlotsX.jl
@@ -1,5 +1,9 @@
 module PGFPlotsX
 
+if isdefined(Base, :Experimental) && isdefined(Base.Experimental, Symbol("@optlevel"))
+    @eval Base.Experimental.@optlevel 1
+end
+
 using ArgCheck: @argcheck
 using Dates
 using DataStructures: OrderedDict
