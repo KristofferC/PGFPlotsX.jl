0c62e4b94b95cbafe215d561f8b342935bcb2ed4
Tamas K. Papp <tkpapp@gmail.com>
2021-04-22 15:08:54 +0200

    also have full TeXLive in docs

--- test/runtests.jl
+++ test/runtests.jl
@@ -34,15 +34,3 @@ include("test_elements.jl")
 mktempdir() do tmp; cd(tmp) do
     include("test_build.jl")
 end end
-
-# Build the docs on Julia v1.3
-if get(ENV, "TRAVIS_JULIA_VERSION", nothing) == "1.5"
-    cd(joinpath(@__DIR__, "..")) do
-        withenv("JULIA_LOAD_PATH" => nothing) do
-            cmd = `$(Base.julia_cmd()) --depwarn=no --color=yes --project=docs/`
-            coverage = Base.JLOptions().code_coverage == 0 ? "none" : "user"
-            run(`$(cmd) -e 'using Pkg; Pkg.instantiate()'`)
-            run(`$(cmd) --code-coverage=$(coverage) docs/make.jl`)
-        end
-    end
-end
